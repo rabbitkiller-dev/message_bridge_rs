0330a9b356511c4cbfe952c663a7ac25ee7162ff
dongRogen <3601778801@qq.com>
2023-03-12 00:18:56 +0800

    feat: 调整rq配置

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,16 @@
 #![allow(non_snake_case)]
 
+use proc_qq::Authentication;
+use proc_qq::re_exports::ricq::version;
 use serde::Deserialize;
 use serde::Serialize;
 use std::fs;
 
 #[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub struct Config {
+    /// 是否将二维码打印到终端
+    #[serde(rename = "printQR")]
+    pub print_qr: Option<bool>,
     #[serde(rename = "qqConfig")]
     pub qq_config: QQConfig,
     #[serde(rename = "discordConfig")]
@@ -27,9 +32,52 @@ impl Config {
 
 #[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
 pub struct QQConfig {
-    pub verifyKey: String,
-    pub host: String,
-    pub port: u32,
+    /// bot账号
+    pub botId: Option<i64>,
+    /// bot登录密码（可选）
+    pub password: Option<String>,
+    version: String,
+    /// 登录认证方式（无token时）
+    auth: String,
+}
+impl QQConfig {
+    /// 获取认证方式
+    pub fn get_auth(&self) -> anyhow::Result<Authentication> {
+        use Authentication::*;
+        match &*self.auth.to_lowercase() {
+            "pwd" => {
+                if self.botId.is_none() || self.password.is_none() {
+                    return Err(anyhow::anyhow!("[QQ] 需配置账号(botId)密码(password)！"));
+                }
+                let pwd = self.password.as_ref().unwrap();
+                if pwd.len() != 16 {
+                    return Err(anyhow::anyhow!("[QQ] 密码请使用16位MD5加密"));
+                }
+                let mut buf = [0; 16];
+                let mut x = 0;
+                for b in pwd.bytes() {
+                    if x > 15 {break}
+                    buf[x] = b;
+                    x += 1;
+                }
+                Ok(UinPasswordMd5(self.botId.unwrap(), buf))
+            }
+            "qr" => Ok(QRCode),
+            _ => Err(anyhow::anyhow!("[QQ] 登录方式目前仅支持：二维码(qr)、账号密码(pwd)")),
+        } // match
+    }
+    /// 获取客户端协议
+    pub fn get_version(&self) -> anyhow::Result<&'static version::Version> {
+        use proc_qq::re_exports::ricq::version::*;
+        match &*self.version.to_lowercase() {
+            "ipad" => Ok(&IPAD),
+            "macos" => Ok(&MACOS),
+            "qidian" => Ok(&QIDIAN),
+            "androidphone" => Ok(&ANDROID_PHONE),
+            "androidwatch" => Ok(&ANDROID_WATCH),
+            v => Err(anyhow::anyhow!("[QQ] 暂不支持[{v}]协议，请更换！")),
+        } // match
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug, Eq, PartialEq)]
